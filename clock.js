be33d69bd836b07bf9b85ec05b98d574552bd8bd
Paeng <gwpaeng@naver.com>
2020-07-28 15:21:43 +0000

    greeting

--- clock.js
+++ clock.js
@@ -3,26 +3,16 @@ const clockContainer = document.querySelector(".js-clock"),
 
 const TIME_LS = "period";
 
-function formatTime(time){
+function rangeTime(time){
     return time < 10 ? `0${time}` : time;
 }
 
 function getTime(){
     const date = new Date();
-    const hours = formatTime(date.getHours());
-    const minutes = formatTime(date.getMinutes());
-    const seconds = formatTime(date.getSeconds());
+    const hours = rangeTime(date.getHours());
+    const minutes = rangeTime(date.getMinutes());
+    const seconds = rangeTime(date.getSeconds());
     clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
-    numberedHour = parseInt(hours);
-    if (numberedHour >= 6 && numberedHour< 12){
-        localStorage.setItem(TIME_LS,"morning");
-    }else if(numberedHour>= 12 && numberedHour< 18){
-        localStorage.setItem(TIME_LS, "afternoon");
-    }else if(numberedHour>= 18 && numberedHour<21){
-        localStorage.setItem(TIME_LS,"evening");
-    }else{
-        localStorage.setItem(TIME_LS,"night");
-    }
 }
 
 function init(){
